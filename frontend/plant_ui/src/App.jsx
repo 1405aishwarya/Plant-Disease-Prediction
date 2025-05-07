59cb739eff5edcb93d2b3f15c9eb0b33752d7f88
Aishwarya <aishwaryaj2020@gmail.com>
2025-05-08 00:46:41 +0530

    ip addr

--- frontend/plant_ui/src/App.jsx
+++ frontend/plant_ui/src/App.jsx
@@ -52,7 +52,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await axios.post("http:// 52.90.29.115:5000/predict", formData, {
+      const response = await axios.post("http://98.81.191.197:5000/predict", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
